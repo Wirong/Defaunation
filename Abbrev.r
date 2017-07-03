b0b597458704ed7a40064e6f88dba2f4c68642ad
Wirong Chanthorn <fsciwrc@ku.ac.th>
2017-07-03 15:47:17 +0800

    Delete Abbrev.r

--- Abbrev.r
+++
@@ -1,5 +0,0 @@
-# Function for making the mnemonic
-Abbrev <-function(dat){
-  Gen_Spp<- (unlist(strsplit(as.character(dat),split=" ")))
- return(paste(substr(Gen_Spp[1],1,3),substr(Gen_Spp[2],1,3),sep=""))
-}
